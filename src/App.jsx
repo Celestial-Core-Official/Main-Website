f6e15181baa98af5eb929961fbc724316296f75f
= <dzolbusines@gmail.com>
2026-05-18 14:44:42 +0200

    feat: remove /games route from application

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import Layout from './components/layout/Layout.jsx'
 import Home from './pages/Home.jsx'
-import Games from './pages/Games.jsx'
 import Projects from './pages/Projects.jsx'
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
@@ -16,7 +15,6 @@ export default function App() {
       <AnimatePresence mode="wait" initial={false}>
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
-          <Route path="/games" element={<Games />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
